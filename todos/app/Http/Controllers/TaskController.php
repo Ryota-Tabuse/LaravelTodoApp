3246d4d585ef1c3eaa6fc059646218d147e04032
ryota-tabuse <bububutase.0107@gmail.com>
2019-10-27 17:47:11 +0900

    フォルダに紐付くタスク一覧機能作成

--- todos/app/Http/Controllers/TaskController.php
+++ todos/app/Http/Controllers/TaskController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Folder;
+use App\Task;
+
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -10,11 +12,19 @@ class TaskController extends Controller
 	//
 	public function index(int $id)
 	{
+		//フォルダの全取得
 		$folders = Folder::all();
+
+		//選択されたフォルダの取得
+		$current_folder = Folder::find($id);
+
+		//選ばれたフォルダにひもづくタスクを取得。
+		$tasks = Task::where('folder_id', $current_folder->id)->get();
 		
 		return view('tasks/index',[
 			'folders' => $folders,
 			'current_folder_id' => $id,
+			'tasks' => $tasks,
 		]);	
 	}
 
